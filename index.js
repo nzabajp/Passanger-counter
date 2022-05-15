06594a6f537b078f43d1ab1397f385387727a2ec
Jean <nzabajp@gmail.com>
2022-05-15 21:30:18 +0200

    js update

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 // document.getElementById("count-el").innerText = 5;
 
 let count = 5;
-
-//
-let bonusPoints = 50;
-console.log(bonusPoints);
-bonusPoints = bonusPoints + 50;
-console.log(bonusPoints)
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
