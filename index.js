8d0fbfa69737dd3fc02ccb4798f69db76d90da6a
Jean Claude Nzabanterura <nzabajp@gmail.com>
2022-05-16 23:43:37 +0200

    Day 2

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-// document.getElementById("count-el").innerText = 5;
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+//change the count-el in the html to reflect the new count
+let count = 0;
+function increment(){
+    count ++;
+    //gets element from with Id: count-el from HTML file and sets
+    //the innerText to count.
+    document.getElementById("count-el").innerText = count;
+}
 
-let count = 5;
+/*
+let countEl = document.getElementById("count-el");
+
+countEl.innerText = count;*/
