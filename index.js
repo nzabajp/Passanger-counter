0603ecf6efd2ab4c9498f066247373119eae678a
Jean Claude Nzabanterura <nzabajp@gmail.com>
2022-07-10 15:08:29 +0200

    code update

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-// initialize the count as 0
 let count = 0;
-// listen for clicks on the increment button
 function increment(){
-    // increment the count variable when the button is clicked
     count ++;
-    //change the count-el in the html to reflect the new count
-    //gets element from with Id: "count-el" from HTML file and sets
-    //the innerText to count.
     document.getElementById("count-el").textContent = count;
 }
-//create a function called save(), which logs out the count when it's called
+
 let savedArr = [];
-let saved;
+
 function save(){
-    saved = count + ", ";
     savedArr.push(count);
-    document.getElementById("save-el").textContent += saved;
+    document.getElementById("save-el").textContent = `Previous entries: ${savedArr.join(" - ")}`;
     count = 0;
     document.getElementById("count-el").textContent = count;
 }
-//returns the total passangers
-let sumArr;
+
 let total = () => {
-    sumArr = savedArr.reduce((x,y) => x + y);
+    const sumArr = savedArr.reduce((x,y) => x + y);
     document.getElementById("total-el").textContent = `Total entered: ${sumArr}`;
 }
+
 let reset = () => {
-    document.location.reload();
+    count = 0
+    savedArr = []
+    document.getElementById("count-el").textContent = count
+    document.getElementById("save-el").textContent = "Previous entries:"
+    document.getElementById("total-el").textContent = "Total entered:"
 }
